639ce1078956c938368e490d04098f4e902b4477
Aidana <aidanasoww@icloud.com>
2025-01-21 01:16:36 +0600

    refactor: xxxxxxxxx

--- src/pages/Form/hooks/useFormLogic.js
+++ src/pages/Form/hooks/useFormLogic.js
@@ -95,7 +95,7 @@ export const useFormLogic = (language) => {
             setState(initialState);
             setOpen(true);
             const link = `https://wa.me/${whatsappNumber}?text=${t("form.message")+message}`;
-            window.open(link, "_blank");
+            window.location.href=link;
         } catch (error) {
             console.error("Error submitting form", error);
         }
