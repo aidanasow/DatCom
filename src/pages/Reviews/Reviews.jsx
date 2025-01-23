61ee04f7c27970af9901d7ef9b0ab3f5313588b4
Aidana <aidanasoww@icloud.com>
2025-01-23 22:16:42 +0600

    fix: videoslider

--- src/pages/Reviews/Reviews.jsx
+++ src/pages/Reviews/Reviews.jsx
@@ -1,136 +1,133 @@
-import {useState } from "react";
-import { Typography, Container, ReviewCard, Breadcrumbs } from "ui/index";
+import {useState} from "react";
+import {Typography, Container, ReviewCard, Breadcrumbs} from "ui/index";
 import classes from "./Reviews.module.scss";
-import { StarIcon, StarEmptyIcon } from "assets/index";
-import { useReviewsStore } from "./store/useReviewsStore";
-import { Slider, PaginationComponent } from "modules/index";
+import {StarIcon, StarEmptyIcon} from "assets/index";
+import {useReviewsStore} from "./store/useReviewsStore";
+import {Slider, PaginationComponent} from "modules/index";
 import ReactPlayer from "react-player";
-import { Loader } from "..";
+import {Loader} from "..";
 import {useTranslation} from "react-i18next";
 import {useMediaQuery} from "utils/helpers/useMedia.js";
 
 export const Reviews = () => {
-  const {t}=useTranslation();
-  const [offset, setOffset] = useState(0);
-  const limit = 6;
+    const {t} = useTranslation();
+    const [offset, setOffset] = useState(0);
+    const limit = 6;
 
-  const { reviews, count, video, rating, loading } = useReviewsStore(
-    offset,
-    limit
-  );
+    const {reviews, count, video, rating, loading} = useReviewsStore(
+        offset,
+        limit
+    );
 
-  const tablet = useMediaQuery("(max-width: 900px)")
-  const phone = useMediaQuery("(max-width: 500px)")
-  const miniTab = useMediaQuery("(max-width: 750px)")
-  const laptop = useMediaQuery("(max-width: 1200px)")
-  let cards = 3
-  if (laptop) cards = 2.5
-  if (tablet) cards = 1.72
-  if (miniTab) cards = 1.8
-  if (phone) cards = 1.5
-  let space =20;
-  if (tablet) space=16;
+    const tablet = useMediaQuery("(max-width: 900px)")
+    const phone = useMediaQuery("(max-width: 500px)")
+    const miniTab = useMediaQuery("(max-width: 750px)")
+    const laptop = useMediaQuery("(max-width: 1200px)")
+    let cards = 3
+    if (laptop) cards = 2.5
+    if (tablet) cards = 1.72
+    if (miniTab) cards = 1.8
+    if (phone) cards = 1.5
+    let space = 20;
+    if (tablet) space = 16;
 
-  const onChange = (_, page) => {
-    setOffset((page - 1) * limit);
-  };
+    const onChange = (_, page) => {
+        setOffset((page - 1) * limit);
+    };
 
 
-  if (loading) {
-    return <Loader />;
-  }
+    if (loading) {
+        return <Loader/>;
+    }
 
-  return (
-      <>
-        <Breadcrumbs breadcrumbKey={"reviews"}/>
-        <div className={classes.wrapper}>
-          <Container>
-            <Typography variant="heading">{t("titles.reviews")}</Typography>
-            <Typography variant="h3" weight="semiBold" className={classes.semiText}>
-              {t("titles.reviewsDet")}
-            </Typography>
-            <div className={classes.reviewWrapper}>
-              <div className={classes.starWrapper}>
-                {Array.from({length: 5}).map((_, index) =>
-                    index < rating?.average ? (
-                        <div key={index} className={classes.star}>
-                          <StarIcon/>
+    return (
+        <>
+            <Breadcrumbs breadcrumbKey={"reviews"}/>
+            <div className={classes.wrapper}>
+                <Container>
+                    <Typography variant="heading">{t("titles.reviews")}</Typography>
+                    <Typography variant="h3" weight="semiBold" className={classes.semiText}>
+                        {t("titles.reviewsDet")}
+                    </Typography>
+                    <div className={classes.reviewWrapper}>
+                        <div className={classes.starWrapper}>
+                            {Array.from({length: 5}).map((_, index) =>
+                                index < rating?.average ? (
+                                    <div key={index} className={classes.star}>
+                                        <StarIcon/>
+                                    </div>
+                                ) : (
+                                    <div key={index} className={classes.star}>
+                                        <StarEmptyIcon/>
+                                    </div>
+                                )
+                            )}
                         </div>
-                    ) : (
-                        <div key={index} className={classes.star}>
-                          <StarEmptyIcon/>
+                        <div className={classes.reviewData}>
+                            <Typography weight="regular">{rating?.average}</Typography>
+                            <Typography color="gray2">{reviews.length} {t("titles.ratings")}</Typography>
                         </div>
-                    )
-                )}
-              </div>
-              <div className={classes.reviewData}>
-                <Typography weight="regular">{rating?.average}</Typography>
-                <Typography color="gray2">{reviews.length} {t("titles.ratings")}</Typography>
-              </div>
-            </div>
+                    </div>
 
-            <div className={classes.cardWrapper}>
-              {reviews.map((item, key) => (
-                  <ReviewCard data={item} key={key}/>
-              ))}
-            </div>
+                    <div className={classes.cardWrapper}>
+                        {reviews.map((item, key) => (
+                            <ReviewCard data={item} key={key}/>
+                        ))}
+                    </div>
 
-            <PaginationComponent count={count} onChange={onChange}/>
-          </Container>
-            <div className={classes.youtubeWrapper}>
-
-                <Typography variant="heading">{t("titles.videoReviews")}</Typography>
-
-              <div>
-                <Slider
-                    maxCards={cards}
-                    spaceBetWeen={space}
-                    sliderList={video}
-                    renderSlide={(item) => (
-                        <div className={classes.youtube}>
-                          <ReactPlayer
-                              width="100%"
-                              height="100%"
-                              style={{
-                                objectFit: "cover"
-                              }}
-                              url={item.link}
-                              playIcon={
-                                <svg
-                                    width="52"
-                                    height="36"
-                                    viewBox="0 0 52 36"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                >
-                                  <g clipPath="url(#clip0)">
-                                    <path
-                                        d="M50.0697 5.615C49.7761 4.52971 49.2032 3.54029 48.4082 2.74526C47.6132 1.95022 46.6238 1.37732 45.5385 1.0836C41.5653 0 25.5745 0 25.5745 0C25.5745 0 9.58293 0.0327999 5.60973 1.1164C4.52443 1.41014 3.53501 1.98307 2.74001 2.77814C1.94502 3.57321 1.37217 4.56267 1.07853 5.648C-0.123273 12.7076 -0.589473 23.4648 1.11153 30.242C1.4052 31.3273 1.97806 32.3167 2.77306 33.1117C3.56805 33.9068 4.55745 34.4797 5.64273 34.7734C9.61593 35.857 25.6071 35.857 25.6071 35.857C25.6071 35.857 41.5981 35.857 45.5711 34.7734C46.6564 34.4797 47.6459 33.9068 48.4409 33.1118C49.2359 32.3168 49.8088 31.3273 50.1025 30.242C51.3701 23.1724 51.7607 12.4218 50.0697 5.615Z"
-                                        fill="#E11414"
-                                    />
-                                    <path
-                                        d="M20.4849 25.6118L33.7505 17.9282L20.4849 10.2446V25.6118Z"
-                                        fill="white"
-                                    />
-                                  </g>
-                                  <defs>
-                                    <clipPath id="clip0">
-                                      <rect width="51.201" height="36"/>
-                                    </clipPath>
-                                  </defs>
-                                </svg>
-                              }
-                              light
-                              playing
-                              controls
-                          />
+                    <PaginationComponent count={count} onChange={onChange}/>
+                    <div className={classes.youtubeWrapper}>
+                        <Typography variant="heading">{t("titles.videoReviews")}</Typography>
+                        <div>
+                            <Slider
+                                maxCards={cards}
+                                spaceBetWeen={space}
+                                sliderList={video}
+                                renderSlide={(item) => (
+                                    <div className={classes.youtube}>
+                                        <ReactPlayer
+                                            width="100%"
+                                            height="100%"
+                                            style={{
+                                                objectFit: "cover"
+                                            }}
+                                            url={item.link}
+                                            playIcon={
+                                                <svg
+                                                    width="52"
+                                                    height="36"
+                                                    viewBox="0 0 52 36"
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                >
+                                                    <g clipPath="url(#clip0)">
+                                                        <path
+                                                            d="M50.0697 5.615C49.7761 4.52971 49.2032 3.54029 48.4082 2.74526C47.6132 1.95022 46.6238 1.37732 45.5385 1.0836C41.5653 0 25.5745 0 25.5745 0C25.5745 0 9.58293 0.0327999 5.60973 1.1164C4.52443 1.41014 3.53501 1.98307 2.74001 2.77814C1.94502 3.57321 1.37217 4.56267 1.07853 5.648C-0.123273 12.7076 -0.589473 23.4648 1.11153 30.242C1.4052 31.3273 1.97806 32.3167 2.77306 33.1117C3.56805 33.9068 4.55745 34.4797 5.64273 34.7734C9.61593 35.857 25.6071 35.857 25.6071 35.857C25.6071 35.857 41.5981 35.857 45.5711 34.7734C46.6564 34.4797 47.6459 33.9068 48.4409 33.1118C49.2359 32.3168 49.8088 31.3273 50.1025 30.242C51.3701 23.1724 51.7607 12.4218 50.0697 5.615Z"
+                                                            fill="#E11414"
+                                                        />
+                                                        <path
+                                                            d="M20.4849 25.6118L33.7505 17.9282L20.4849 10.2446V25.6118Z"
+                                                            fill="white"
+                                                        />
+                                                    </g>
+                                                    <defs>
+                                                        <clipPath id="clip0">
+                                                            <rect width="51.201" height="36"/>
+                                                        </clipPath>
+                                                    </defs>
+                                                </svg>
+                                            }
+                                            light
+                                            playing
+                                            controls
+                                        />
+                                    </div>
+                                )}
+                            />
                         </div>
-                    )}
-                />
-              </div>
+                    </div>
+                </Container>
             </div>
+        </>
 
-        </div>
-      </>
-
-  );
+    );
 };
