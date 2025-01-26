7ed8c7376700be3fd7b3e5c1a61e5859241d9b4c
Aidana <aidanasoww@icloud.com>
2025-01-26 15:37:12 +0600

    hz

--- src/pages/Reviews/components/YouTubeVideo.jsx
+++ src/pages/Reviews/components/YouTubeVideo.jsx
@@ -1,6 +1,10 @@
-export const YouTubeVideo = ({ videoId }) => {
+import { useState } from "react";
+import {PlayIcon} from "assets/icons/PlayIcon.jsx";
 
-    const getVideoId = url => {
+export const YouTubeVideo = ({ videoId }) => {
+    const [showVideo, setShowVideo] = useState(false);
+
+    const getVideoId = (url) => {
         try {
             const parsedUrl = new URL(url);
             const videoIdFromParam = parsedUrl.searchParams.get("v");
@@ -10,20 +14,55 @@ export const YouTubeVideo = ({ videoId }) => {
             throw new Error(error);
         }
     };
-    return (
-        <div style={{width: "100%",height: "100%" }}>
-            <iframe
-                src={`https://www.youtube.com/embed/${getVideoId(videoId)}`}
-                title="YouTube video"
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-                style={{
 
-                    width: "100%",
-                    height: "100%"
-                }}
-            ></iframe>
+    return (
+        <div
+            style={{
+                width: "100%",
+                height: "100%",
+                position: "relative",
+                cursor: "pointer",
+            }}
+            onClick={() => setShowVideo(true)}
+        >
+            {showVideo ? (
+                <iframe
+                    src={`https://www.youtube.com/embed/${getVideoId(videoId)}?autoplay=1`}
+                    title="YouTube video"
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                    playsInline
+                    style={{
+                        width: "100%",
+                        height: "100%",
+                    }}
+                ></iframe>
+            ) : (
+                <img
+                    src={`https://img.youtube.com/vi/${getVideoId(videoId)}/hqdefault.jpg`}
+                    alt="Video thumbnail"
+                    style={{
+                        width: "100%",
+                        height: "100%",
+                        objectFit: "cover",
+                    }}
+                />
+            )}
+
+            {!showVideo && (
+                <div
+                    style={{
+                        position: "absolute",
+                        top: "50%",
+                        left: "50%",
+                        transform: "translate(-50%, -50%)",
+
+                    }}
+                >
+                   <PlayIcon/>
+                </div>
+            )}
         </div>
     );
 };
