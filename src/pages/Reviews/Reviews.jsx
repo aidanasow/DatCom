3bf1237525a7d1356e382cf40ce52749bf440b7d
Aidana <aidanasoww@icloud.com>
2025-01-26 13:49:49 +0600

    fix: videoslider

--- src/pages/Reviews/Reviews.jsx
+++ src/pages/Reviews/Reviews.jsx
@@ -91,7 +91,7 @@ export const Reviews = () => {
                                             <PlayIcon/>
                                         }
                                         controls
-                                        light
+                                        playing
                                     />
                                 </div>
                             )}
